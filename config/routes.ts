21bb8446370fe13334a761175946a404f6a76f3a
hanhuikrkr <1084402065@qq.com>
2021-02-05 23:13:27 +0800

    更新分类

--- config/routes.ts
+++ config/routes.ts
@@ -14,7 +14,6 @@ export default [
           },
           {
             name: '注册页',
-            icon: 'smile',
             path: '/user/userregister',
             component: './User/UserRegister',
           },
@@ -56,35 +55,97 @@ export default [
                   {
                     path: '/admin/sub-page',
                     name: 'sub-page',
-                    icon: 'smile',
+
                     component: './AlertDocList',
                     authority: ['admin'],
                   },
                   {
                     path: '/admin/uploadDoc',
                     name: '上传文档',
-                    icon: 'smile',
                     component: './UploadDoc/index.tsx',
                     authority: ['admin'],
                   },
                 ],
               },
-             
+
               {
                 name: 'JavaScript',
-                icon: 'smile',
                 path: '/javascript',
                 component: './DocList',
+                routes: [
+                  {
+                    name: 'JS 中万物皆对象',
+                    path: '/javascript/jsobject?sort=110100',
+                    component: './DocList',
+                  },
+                  {
+                    name: '原型与继承',
+                    path: '/javascript/jsobject?sort=110200',
+                    component: './DocList',
+                  },
+                  {
+                    name: 'proxy代理',
+                    path: '/javascript/jsobject?sort=110300',
+                    component: './DocList',
+                  },
+                  {
+                    name: '基本引用类型',
+                    path: '/javascript/jsobject?sort=110400',
+                    component: './DocList',
+                  },
+                  {
+                    name: '基础知识',
+                    path: '/javascript/jsobject?sort=110500',
+                    component: './DocList',
+                  },
+                  {
+                    name: '迭代器与生成器',
+                    path: '/javascript/jsobject?sort=110600',
+                    component: './DocList',
+                  },
+                  {
+                    name: 'Promise与async',
+                    path: '/javascript/jsobject?sort=110700',
+                    component: './DocList',
+                  },
+                  {
+                    name: '模块',
+                    path: '/javascript/jsobject?sort=110800',
+                    component: './DocList',
+                  },
+                  {
+                    name: 'BOM和DOM傻傻分不清',
+                    path: '/javascript/jsobject?sort=110900',
+                    component: './DocList',
+                  },
+                  {
+                    name: 'JS里的坑',
+                    path: '/javascript/jsobject?sort=111000',
+                    component: './DocList',
+                  },
+                  {
+                    name: '网络请求',
+                    path: '/javascript/jsobject?sort=111200',
+                    component: './DocList',
+                  },
+                  {
+                    name: '事件',
+                    path: '/javascript/jsobject?sort=111300',
+                    component: './DocList',
+                  },{
+                    name: '手写时间',
+                    path: '/javascript/jsobject?sort=111100',
+                    component: './DocList',
+                  },
+                ],
               },
               {
                 name: '计算机网络',
-                icon: 'smile',
                 path: '/network',
                 component: './DocList',
                 routes: [
                   {
                     name: 'TCP',
-                    icon: 'smile',
                     path: '/Network/Tcp?sort=110101',
                     component: './DocList',
                   },
@@ -92,7 +153,6 @@ export default [
               },
               {
                 name: '个人项目',
-                icon: 'smile',
                 path: '/project',
                 component: './ProjectPage',
               },
